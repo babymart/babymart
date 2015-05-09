1a531734252bbe703a067e774fa31607627ead30
babymart <www.babymart.vn@gmail.com>
2015-05-09 15:29:26 +0700

    first

---
+++ Babymart/Global.asax.cs
@@ -0,0 +1,85 @@
+﻿using Babymart.DAL.Product;
+using Babymart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace Babymart
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            try
+            {
+                AreaRegistration.RegisterAllAreas();
+                WebApiConfig.Register(GlobalConfiguration.Configuration);
+                FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+                RouteConfig.RegisterRoutes(RouteTable.Routes);
+                BundleConfig.RegisterBundles(BundleTable.Bundles);
+                AuthConfig.RegisterAuth();
+                GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            }
+            catch
+            {
+            }
+
+        }
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            if (FormsAuthentication.CookiesSupported == true)
+            {
+                if (Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+                {
+                    try
+                    {
+                        //let us take out the username now                
+                        string username = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
+                        string roles = string.Empty;
+
+                        using (shopdochoi123_dbEntities1 entities = new shopdochoi123_dbEntities1())
+                        {
+                            admin user = entities.admins.SingleOrDefault(u => u.username == username);
+
+                            roles = user.Roles;
+                        }
+                        //let us extract the roles from our own custom cookie
+
+
+                        //Let us set the Pricipal with our user specific details
+                        HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(
+                          new System.Security.Principal.GenericIdentity(username, "Forms"), roles.Split(';'));
+                    }
+                    catch (Exception)
+                    {
+                        //somehting went wrong
+                    }
+                }
+            }
+        }
+        protected void Application_EndRequest()
+        {
+            if (Context.Response.StatusCode == 404)
+            {
+                if ((!Request.RawUrl.Contains("style")) && (!Request.RawUrl.Contains("images")))
+                {
+                    Response.Clear();
+                    if (Response.StatusCode == 404)
+                    {
+                        Response.Redirect("/404.html");
+                    }
+                }
+            }
+        }
+    }
+}
